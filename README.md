a1b03f2d587fdac587cdda8d6e2baee6b744a10b
Pawan Rama Mali <prm@outlook.in>
2021-07-27 14:10:06 +0530

    Added profiles viewset

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Profiles REST API Demo
 
 Profiles REST API
+
+
+# Tools Used
+
+* Git Bash (To commit changes to GitLab Repository)
+* Atom Code Editor
+* Oracle VM Virtual Box
+* Vagrant (Development Server)
+** Ubuntu/bionic64 OS
+** Python Virtual Environment
+** Django
+** Django REST Framework
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
+
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -45,3 +51,25 @@ class HelloApiView(APIView):
     def delete(self,request,pk=None):
         """Delete an object"""
         return Response({'method':'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self,request):
+        """Return a Hello Message"""
+        a_viewset = [
+            'Uses  actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message':'Hello ! ', 'a_viewset':a_viewset})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles  """
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
